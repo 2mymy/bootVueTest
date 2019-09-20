6f46a59ad03ef8995d27d904ea6f056a712d5295
mymy <rg115@naver.com>
2019-09-20 15:32:14 +0900

    spring boot (jdk8 + gradle) & vue.js setting test

---
+++ back/src/main/java/com/yystd/YystdBootApplication.java
@@ -0,0 +1,13 @@
+package com.yystd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YystdBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YystdBootApplication.class, args);
+	}
+
+}
